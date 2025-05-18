4f95a680b22c1d399c3357dbb7a864c90d3e2ea2
Emerson Connelly <emerconn7@gmail.com>
2025-05-18 15:33:00 -0500

    add fps and coutner

--- main.py
+++ main.py
@@ -8,14 +8,26 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    while True:
-        screen.fill("black")
-        pygame.display.flip()
+    clock = pygame.time.Clock()
+    fps = 60
+    counter = 0
+    dt = 0
 
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black") # fill entire screen black
+        pygame.display.flip() # update contents of entire screen
+
+        clock.tick(fps)
+        dt /= 1000
+        counter += 1
+        if counter == fps:
+            print(f"fps: {clock.get_fps()}")
+            counter = 0
+
 
 if __name__ == "__main__":
     main()
